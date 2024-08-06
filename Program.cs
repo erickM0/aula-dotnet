1cc57ed0fbd358e0010cb7f38dcf11149614f69b
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-06 17:06:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aula_CSharp.Models;
+
+Pessoa pessoa = new Pessoa()
+{
+    Nome = "João",
+    Idade = 19,
+};
+
+
+pessoa.Apresentar();
