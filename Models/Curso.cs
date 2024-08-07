ea3757ca7ce0511c8b0824306dca6f4a433c4a1a
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-07 19:56:21 -0300

    Inclusão da classe Curso e suas propriedade e metodos

---
+++ Models/Curso.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula_CSharp.Models
+{
+    public class Curso
+    {
+        public string Nome {get; set;}
+
+        public List<Pessoa> Alunos { get; set; }
+    
+        public void AdicionarAluno(Pessoa aluno){
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeAlunosMatriculados(){
+            int quntidade =  Alunos.Count;
+
+            return quntidade;
+        }
+
+        public bool RemoverAluno(Pessoa aluno){
+            return Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos(){
+            foreach(Pessoa aluno in Alunos){
+                Console.WriteLine(aluno.NomeCompleto);
+            }
+        }
+    }
+}
