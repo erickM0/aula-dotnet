cb2bee95d29f9a40fafe3785e1c10973deb503ce
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-07 19:57:22 -0300

    utilizando e testando a classe Curso e as alteraçoes na classe Pessoa

--- Program.cs
+++ Program.cs
@@ -1,11 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Aula_CSharp.Models;
 
-Pessoa pessoa = new Pessoa()
+
+Pessoa pessoa = new()
 {
     Nome = "João",
-    Idade = 19,
+    Sobrenome = "Carlos",
+    Idade = 25,
+};
+
+Pessoa pessoa2 = new()
+{
+    Nome = "Leonardo",
+    Sobrenome = "Buta",
+    Idade = 29,
+};
+
+Pessoa pessoa3 = new()
+{
+    Nome = "Maria",
+    Sobrenome = "Diva Macedo",
+    Idade = 60,
 };
 
 
-pessoa.Apresentar();
+Curso dio = new Curso(){
+    Nome = "FullStack",
+};
+
+dio.Alunos = new List<Pessoa>();
+Console.WriteLine(dio.Nome);
+
+dio.AdicionarAluno(pessoa);
+dio.AdicionarAluno(pessoa2);
+dio.AdicionarAluno(pessoa3);
+
+Console.WriteLine(dio.ObterQuantidadeAlunosMatriculados());
+
+dio.ListarAlunos();
+
+if(dio.RemoverAluno(pessoa2)){
+    dio.ListarAlunos();
+}
+
+
+
+
+
