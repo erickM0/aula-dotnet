bd37d24e9b93a4e2f00f784515259fc9b08f256d
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-07 19:55:16 -0300

    inclusão de priedades e validação na classe Pessoa

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,50 @@ namespace Aula_CSharp.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
-        public int Idade { get;set;}
+
+        private string _nome;
+        private int _idade;
+
+
+        public string Nome 
+        { 
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if(value == ""){
+                    throw new ArgumentException("O nome não pode estar vasio!");
+                }
+                
+                    _nome = value;
+                
+                
+            } 
+        }
+
+        public string Sobrenome { get; set; }
+
+        public string NomeCompleto 
+        { 
+            get => $"{Nome} { Sobrenome}".ToUpper();
+        }
+        public int Idade 
+        { 
+            get => _idade;
+            set{
+                if(value < 0){
+                    throw new ArgumentException("A idade não pode ser menor que 0");
+                }
+                _idade = value;
+            }
+        }
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá! Meu nome é {Nome}, tenho {Idade} anos.");
+            Console.WriteLine($"Olá! Meu nome é {NomeCompleto}, tenho {Idade} anos.");
         }
     }
 }
